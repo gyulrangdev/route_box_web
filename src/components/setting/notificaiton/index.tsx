ad5b4af10e770222b02ee6b0ed166b6e928bc4c5
laon6 <laon6.dev@gmail.com>
2025-01-12 16:34:52 +0900

    알림 설정 툴팁 추가

--- src/components/setting/notificaiton/index.tsx
+++ src/components/setting/notificaiton/index.tsx
@@ -3,7 +3,15 @@ import NotificationListItem from './NotificationListItem';
 import { Container } from './style';
 
 const NotificationList: React.FC = () => {
-  const notifications = [{ name: '마케팅 정보 알림' }, { name: '여행 도중 사진 알림', info: true }];
+  const notifications = [
+    { name: '마케팅 정보 알림' },
+    {
+      name: '여행 도중 사진 알림',
+      info: true,
+      tooltipText:
+        '여행 도중,\n특정 위치에서 일정 시간 이상\n시간 머물때, 놓치지 않고 기록\n할 수 있게 알림을 줍니다.',
+    },
+  ];
   return (
     <Container>
       {notifications.map((notification, index) => (
@@ -11,6 +19,7 @@ const NotificationList: React.FC = () => {
           key={index}
           notification={notification.name}
           info={notification.info}
+          tooltipText={notification.tooltipText}
         />
       ))}
     </Container>
