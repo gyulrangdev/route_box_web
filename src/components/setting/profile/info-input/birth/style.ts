368bcc3651b97806d8133f31d2227388fd045634
devGyulMem <khmdevmem@gmail.com>
2025-01-28 20:38:54 +0900

    Refactor: styled-component props 변수 수정 (#feat-68)

--- src/components/setting/profile/info-input/birth/style.ts
+++ src/components/setting/profile/info-input/birth/style.ts
@@ -8,7 +8,7 @@ export const BirthContainer = styled.div`
   justify-content: space-evenly;
 `;
 
-export const BirthSelect = styled.select<{ hasValue: boolean }>`
+export const BirthSelect = styled.select<{ $hasValue: boolean }>`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -16,7 +16,7 @@ export const BirthSelect = styled.select<{ hasValue: boolean }>`
   border: none;
   outline: none;
 
-  color: ${(props) => (props.hasValue ? 'var(--black)' : 'var(--Gray5_outline, #d3d3d3)')};
+  color: ${(props) => (props.$hasValue ? 'var(--black)' : 'var(--Gray5_outline, #d3d3d3)')};
   font-feature-settings: 'case' on;
   font-size: 1.125rem;
   font-style: normal;
